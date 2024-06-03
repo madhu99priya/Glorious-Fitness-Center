c8e1a82b890488b67d6417380c8e6d938bfc3855
madhu99priya <157110469+madhu99priya@users.noreply.github.com>
2024-06-03 22:05:59 +0530

    styling

--- src/data/programsData.jsx
+++ src/data/programsData.jsx
@@ -7,28 +7,28 @@ import { IoFitnessOutline } from "react-icons/io5";
 export const programsData = [
   {
     id: 1,
-    image: <LuDumbbell  size={25}/>,
+    image: <LuDumbbell  size={26}/>,
     heading: "Strength Training",
     details:
       "In this program, you are trained to improve your strength through many exercises.",
   },
   {
     id:2,
-    image: < FaRunning size={25}/>,
+    image: < FaRunning size={26}/>,
     heading: "Cardio Training",
     details:
       "In this program, you are trained to do sequential moves in range of 20 until 30 minutes.",
   },
   {
     id:3,
-    image: <FaFireAlt  size={25}/>,
+    image: <FaFireAlt  size={26}/>,
     heading: "Fat Burning",
     details:
       "This program is suitable for you who wants to get rid of your fat and lose their weight.",
   },
   {
     id: 4,
-    image: <IoFitnessOutline size={25}/>,
+    image: <IoFitnessOutline size={26}/>,
     heading: "Health Fitness",
     details:
       "This programs is designed for those who exercises only for their body fitness not body building.",
